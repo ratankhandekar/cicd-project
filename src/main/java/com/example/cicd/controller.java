bc9581c612ecc3f3756535b81cc7312f731096cd
RatanKhandekar <ratankhandekar2@gmail.com>
2025-03-25 23:15:10 +0530

    second commit

--- src/main/java/com/example/cicd/controller.java
+++ src/main/java/com/example/cicd/controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class controller {
 
-
+    //ratan changesss into branch
     @GetMapping("/home")
     public String home()
     {
